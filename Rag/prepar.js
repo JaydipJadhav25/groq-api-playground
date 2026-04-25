d5ab9ca147ff9585e8bf932e9141ee4f9c150072
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2026-04-25 11:36:05 +0530

    feat:  Chunk the document

--- Rag/prepar.js
+++ Rag/prepar.js
@@ -1,11 +1,22 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import path from "path";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 
 export async function indexDocuments(fileName) {
-     
-    const filePath = path.resolve(fileName);
-    const loader = new PDFLoader(filePath , { splitPages : false});
-    // const docs = await loader.load();
-    const doc = await loader.load();
-    console.log(doc[0].pageContent);
+  const filePath = path.resolve(fileName);
+  const loader = new PDFLoader(filePath, { splitPages: false });
+  // const docs = await loader.load();
+  const doc = await loader.load();
+//   console.log(doc[0].pageContent);
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 500,
+    chunkOverlap: 100,
+  });
+
+  const chunks = await splitter.splitText(doc[0].pageContent);
+
+
+  console.log(chunks.length);
+  console.log(chunks);
 }
